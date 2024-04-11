8c13d3d382eff8ef0b6c92782490e32071cb7eac
Jin <99856344+BlueBloodFire@users.noreply.github.com>
2024-04-11 17:19:47 +0800

    添加身份认证配置文件-1

--- src/main/java/org/wangjin/vvserver/config/SecurityConfig.java
+++ src/main/java/org/wangjin/vvserver/config/SecurityConfig.java
@@ -1,13 +1,30 @@
 package org.wangjin.vvserver.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.ibatis.reflection.wrapper.ObjectWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.*;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.wangjin.vvserver.model.Hr;
+import org.wangjin.vvserver.model.RespBean;
 import org.wangjin.vvserver.service.HrService;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -25,4 +42,62 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth.userDetailsService(hrService);
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .usernameParameter("username")
+                .passwordParameter("password")
+                .loginProcessingUrl("/doLogin")
+                .loginPage("/login")
+                .successHandler(new AuthenticationSuccessHandler() {
+                    @Override
+                    public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
+                        httpServletResponse.setContentType("application/json;charset=UTF-8");
+                        PrintWriter out = httpServletResponse.getWriter();
+                        Hr hr = (Hr) authentication.getPrincipal();
+                        RespBean ok = RespBean.ok("登录成功！",hr);
+                        String s = new ObjectMapper().writeValueAsString(ok);
+                        out.write(s);
+                        out.flush();
+                        out.close();
+                    }
+                })
+                .failureHandler(new AuthenticationFailureHandler() {
+                    @Override
+                    public void onAuthenticationFailure(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
+                        httpServletResponse.setContentType("application/json;charset=UTF-8");
+                        PrintWriter out = httpServletResponse.getWriter();
+                        RespBean respBean = RespBean.error("登录失败！");
+                        if (e instanceof LockedException) {
+                            respBean.setMsg("账户被锁定，请联系王津！");
+                        } else if (e instanceof CredentialsExpiredException) {
+                            respBean.setMsg("密码过期，请联系王津！");
+                        } else if (e instanceof AccountExpiredException) {
+                            respBean.setMsg("账户过期，请联系王津！");
+                        } else if (e instanceof DisabledException) {
+                            respBean.setMsg("账户被禁用，请联系王津！");
+                        } else if (e instanceof BadCredentialsException) {
+                            respBean.setMsg("用户名或者密码输入错误！");
+                        }
+                        out.write(new ObjectMapper().writeValueAsString(respBean));
+                        out.flush();
+                        out.close();
+                    }
+                })
+                .permitAll()
+                .and()
+                .logout()
+                .logoutSuccessHandler(new LogoutSuccessHandler() {
+                    @Override
+                    public void onLogoutSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
+
+                    }
+                })
+                .permitAll()
+                .and()
+                .csrf().disable();
+    }
 }
